b424840f8357a4cbb90d7e58f7f6c5e8084f0f68
fapeg <fabian@fapeg.com>
2014-05-07 02:59:08 +0200

    ein- und ausloggen eingebaut

--- main.py
+++ main.py
@@ -2,21 +2,23 @@
 # Autor: Giulia Kirstein, Daniel Gros, Fabian Pegel
 # Mai 2014
 # Projektseminar Angewandte Informationswissenschaft                      
-from flask import Flask, render_template, g, flash, redirect
+from flask import Flask, render_template, g, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import datetime, hashlib, os, re
 from flask.ext.wtf import Form
 from flask.ext.login import LoginManager
-from wtforms import TextField, BooleanField
+from wtforms import TextField, BooleanField, PasswordField
 from wtforms.validators import Required
-
-
+from flask.ext.login import login_user, logout_user, current_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jHgFtjjGFdE5678ijbDDegh'
 app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/fabian/adressbuch/adressen.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///adressen.sqlite'
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+
 
 class User(db.Model):
   # Setting the table name and
@@ -26,6 +28,20 @@ class User(db.Model):
   verknuepfte_daten_id = db.Column(db.Integer)
   passwort = db.Column(db.String(100))
   username = db.Column(db.String(200))
+  def is_authenticated(self):
+      return True
+
+  def is_active(self):
+      return True
+
+  def is_anonymous(self):
+      return False
+
+  def get_id(self):
+      return unicode(self.id)
+
+  def __repr__(self):
+      return '<User %r>' % (self.username)
   
   def __init__(self, id, verknuepfte_daten_id, passwort, username):
       # Initializes the fields with entered data
@@ -34,6 +50,12 @@ class User(db.Model):
       self.verknuepfte_daten_id = verknuepfte_daten_id
       self.passwort = passwort
       self.username = username
+	  
+	  
+	  
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Entry(db.Model):
   # Setting the table name and
@@ -76,7 +98,7 @@ class Entry(db.Model):
 
 class LoginForm(Form):
     username = TextField('username', validators = [Required("Bitte einen Benutzernamen eingeben!")])
-    password = TextField('password', validators = [Required("Bitte ein Passwort eingeben!")]) 
+    password = PasswordField('password', validators = [Required("Bitte ein Passwort eingeben!")]) 
 	
     remember_me = BooleanField('remember_me', default = False)
 
@@ -87,6 +109,11 @@ def hello_world():
 	entries=Entry.query.with_entities(Entry.vorname,Entry.name,Entry.titel,Entry.strasse,Entry.plz,Entry.ort,Entry.geburtsdatum,Entry.festnetz,Entry.mobil,Entry.email,Entry.homepage,Entry.twitter)    
 	return render_template('anzeige.htm', entries=entries) 
 	
+    
+@app.route('/edit')
+@login_required
+def edit():
+    return render_template('edit.htm') 
 	
 	
 @app.route('/login', methods = ['GET', 'POST'])
@@ -94,10 +121,32 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user="' + form.username.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/')
+        p_username = form.username.data
+        p_password = form.password.data
+        remember_me = False
+        if 'remember_me' in session:
+            remember_me = session['remember_me']
+            session.pop('remember_me', None)
+        user = User.query.filter_by(username=p_username).first()
+        if user is not None and user.passwort == hashlib.md5(p_password).hexdigest():
+            session['username']=user.username
+            login_user(user, remember = remember_me)
+            return redirect('/')
+        else:
+            flash('Benutzername oder Passwort falsch!')
+
     return render_template('login.htm', 
         title = 'Sign In',
         form = form)
+        
+@app.route('/logout')
+def logout():
+    if session['username']:
+        session.pop('username', None)
+    logout_user()
+    return redirect('/login')
+
+        
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0')     
